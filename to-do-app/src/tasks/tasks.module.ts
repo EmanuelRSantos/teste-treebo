fedcd5e8406d4dfedb46f714b90146de201ca76a
EmanuelRSantos <rodriguezemanuel6411@gmail.com>
2024-07-03 17:11:46 -0300

    T002: build - Importado DatabaseModule para configuração do banco de dados e tasksProviders

--- to-do-app/src/tasks/tasks.module.ts
+++ to-do-app/src/tasks/tasks.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { tasksProviders } from './tasks.provider';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService,...tasksProviders],
 })
 export class TasksModule {}
