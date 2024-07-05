4cf14b84eb94004f24b55ff9e3f71ecd142aa45f
EmanuelRSantos <rodriguezemanuel6411@gmail.com>
2024-07-05 17:40:31 -0300

    T004: style - Estilizando todo o front usando a biblioteca TailWind

--- to-do-app-front/src/components/taskForm.jsx
+++ to-do-app-front/src/components/taskForm.jsx
@@ -32,28 +32,32 @@ const TaskForm = ({ taskToEdit, clearEdit }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Tarefa:</label>
+        <form onSubmit={handleSubmit} className="bg-white p-6 rounded-md shadow-md">
+            <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">Tarefa:</label>
                 <input
                     type="text"
-                    name='task'
+                    name="task"
                     value={task.task}
                     onChange={handleChange}
                     required
+                    className="w-full p-2 border border-gray-300 rounded-md"
                 />
             </div>
 
-            <div>
-                <label>Descrição: </label>
+            <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2">Descrição: </label>
                 <textarea
                     name="description"
                     value={task.description}
                     onChange={handleChange}
                     required
+                    className="w-full p-2 border border-gray-300 rounded-md"
                 ></textarea>
             </div>
-            <button type="submit">Salvar Tarefa</button>
+
+            <button className="inline-flex rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                type="submit">Salvar Tarefa</button>
         </form>
     );
 };
